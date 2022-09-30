6cffe11c3ec0d39996174b39191f35e14bb10f70
AtomsPC <tomullender@gmail.com>
2022-09-30 15:16:00 +0100

    Verti-slice

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using UnManaged;
+
+namespace Mousinator5000
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int WS_EX_TRANSPARENT = 0x00000020;
+        const int GWL_EXSTYLE = (-20);
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+
+        public static void SetWindowExTransparent(IntPtr hwnd)
+        {
+            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WindowStyle = WindowStyle.None;
+            Topmost = true;
+            AllowsTransparency = true;
+            Opacity = 0.2;
+            WindowState = WindowState.Maximized;
+
+
+            var _hotKey = new HotKey(Key.F18, KeyModifier.Shift, OnHotKeyHandler);
+        }
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var hwnd = new WindowInteropHelper(this).Handle;
+            SetWindowExTransparent(hwnd);
+        }
+        private void OnHotKeyHandler(HotKey hotKey)
+        {
+            
+            SetCursorPos(100, 100);
+        }
+    }
+}
